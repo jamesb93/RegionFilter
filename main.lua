bbbc56ebf2d5ec71d645f170b0c2ccca9cc10298
jordanschwab <37817031+jordanschwab@users.noreply.github.com>
2022-03-13 10:35:57 +1300

    Update main.lua
    Updated to 1.5.0 version and refactored to be a bit easier to read / follow closer to original code done by jamesb93.
    
    Removed UI since it didn't play nicely with other custom UI elements. Can only be triggered by using /rfilter now (based on original code from jamesb93).
    
    Issues:
    1. when using the refresh, it won't have access to the region information for new results. You can close/reopen the interface to get the filter to apply correctly.
    2. When looking for groups, the API only returns the100 results. Since you can expect more than that, groups tend to appear and disappear randomly as they come onto and fall off that list.

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local RF = select(2, ...)
 local servers = RF.servers
 local posts = RF.posts
-RF.version = "1.4.0"
+RF.version = "1.5.0"
 RF.togRemove = false
 
 local spaced_realm = string.gsub(GetRealmName(), "%s+", "")
@@ -24,47 +24,58 @@ if RF.region == 'OC' then RF.postType = posts.oc_post end
 if RF.region == 'LA' then RF.postType = posts.la_post end
 if RF.region == 'BR' then RF.postType = posts.br_post end
 ---- Removing Enrties when togRemove is enabled
--- function RF.removeEntries(results)
--- 	if RF.togRemove then
--- 		for i=1, #results do
--- 			local resultID = results[i]
--- 			local searchResults = C_LFGList.GetSearchResultInfo(resultID)
+function RF.removeEntries(results)
+	local isLocal = {}
+	if RF.togRemove then
+		local localCount = 0
+		local originalResultCount = #results
+		for i=originalResultCount, 1, -1 do
+			local resultID = results[i]
+			isLocal[resultID] = false
+			local searchResults = C_LFGList.GetSearchResultInfo(resultID)
 
--- 			local leaderName = searchResults.leaderName
+			local leaderName = searchResults.leaderName
 
--- 			if leaderName ~= nil then -- Filter out nil entries from LFG Pane
--- 				local name, realm = RF:sanitiseName(leaderName)
--- 				local info = servers[realm]
--- 				if info ~= nil then
--- 					local region = info[1]
--- 					if RF.region ~= region then
--- 						table.remove(results, i)
--- 					end
--- 				end
--- 			end
--- 		end
--- 	end
--- 	table.sort(results)
--- 	LFGListFrame.SearchPanel.totalResults = #results
--- 	return true
--- end
+			if leaderName ~= nil then -- Filter out nil entries from LFG Pane
+				local name, realm = RF:sanitiseName(leaderName)
+				local info = servers[realm]
+				if info ~= nil then
+					local region = info[1]
+					if RF.region ~= region then
+						table.remove(results, i)
+					else
+						localCount = localCount + 1
+						isLocal[resultID] = true 
+					end
+				end
+			end
+		end
+--[[ 		local outcomeMessage = '|cff00ffff[Region Filter]: |cffFF6EB4 Found ' ..localCount..'/'..originalResultCount..' groups.'
+		--When refreshing the UI, GetSearchResultInfo will fail to provide information on some groups. 
+		if (#results-localCount > 0) then
+			outcomeMessage = outcomeMessage..'|cffff0000 Info missing from ' .. #results-localCount .. ', refresh to fix.'
+		end
+		print(outcomeMessage) ]]
 
-local function addFilteredId(self, id)
-    if ( not self.filteredIDs ) then
-        self.filteredIDs = { };
-    end
-    tinsert(self.filteredIDs, id);
-end
-
-local function filterTable(t, ids)
-    for i, id in ipairs(ids) do
-        for j = #t, 1, -1 do
-            if ( t[j] == id ) then
-                tremove(t, j);
-                break;
-            end
-        end
-    end
+		local function sortCompFunc(searchResultID1, searchResultID2)
+			if (isLocal[searchResultID1]) then 
+				if (not isLocal[searchResultID2]) then
+					return true
+				end
+			else
+				if (isLocal[searchResultID2]) then 
+					return false
+				end
+			end
+			return searchResultID1>searchResultID2
+		end
+	
+		table.sort(results, sortCompFunc)
+	else
+		table.sort(results)
+	end
+	LFGListFrame.SearchPanel.totalResults = #results
+	return true
 end
 
 ---- Updating the text of entries
@@ -78,126 +89,38 @@ function RF.updateEntries(results)
 		local name, realm = RF:sanitiseName(leaderName)
 		local info = servers[realm]
 		if info then
-			local region, dataCentre = info[1], info[2]
+			local region, dataCentre, regionColour = info[1], info[2], info[3]
 			if region == "NA" then
-				results.ActivityName:SetText(
-					RF:regionTag(
-						RF.region, 
-						region, 
-						region..'-'..dataCentre, 
-						activityName,
-						RF.dataCentre,
-						dataCentre
-					)
-				)
-				results.ActivityName:SetTextColor(
-					RF:dungeonText(RF.region, region)
-				)
+				regionLabel = region..'-'..dataCentre;
 			else
+				regionLabel = region
+			end
 				results.ActivityName:SetText(
 					RF:regionTag(
-						RF.region, 
-						region, 
-						region, 
+						regionLabel, 
 						activityName,
-						nil, nil
+						regionColour
 					)
 				)
 				results.ActivityName:SetTextColor(
 					RF:dungeonText(RF.region, region)
 				)
-			end
 		end
 	end
 end
 
-function RF.sortSearchResults(results) 
-    local categoryID = LFGListFrame.SearchPanel.categoryID;
-	local countRemoved = 0
-	local countRemaining = 0
-	
-	-- RF.region -- filter by this
-	local function FilterSearchResults(searchResultID)
-		if not RF.togRemove then 
-			return 
-		end
 
-		local searchResults = C_LFGList.GetSearchResultInfo(searchResultID)
-		local leaderName = searchResults.leaderName
-		local removedByFilter = true
-
-		if leaderName ~= nil then -- Filter out nil entries from LFG Pane
-			local name, realm = RF:sanitiseName(leaderName)
-			local info = servers[realm]
-			if info then
-				local region, dataCentre = info[1], info[2]
-
-				if (RF.region == region and RF.dataCentre == dataCentre) then
-					removedByFilter = false
-					countRemaining = countRemaining + 1
-				end
-			end
-		end
-
-		if (removedByFilter) then 
-			--print('removing '..searchResultID)
-			--table.remove(results,searchResultID)
-			addFilteredId(LFGListFrame.SearchPanel, searchResultID);
-			countRemoved = countRemoved + 1
-		end
+SLASH_RFILTER1 = "/rfilter"
+SlashCmdList["RFILTER"] = function(msg)
+	if RF.togRemove then
+		print('|cff00ffff[Region Filter]: |cffFF6EB4 Not filtering outside regions')
+	else
+		print('|cff00ffff[Region Filter]: |cffFF6EB4 Filtering outside regions')
 	end
-  
-	if (#results > 0 and categoryID == 2) then
-		for index = #results, 1, -1 do
-			FilterSearchResults(results[index])
-		end
-		
-		if (LFGListFrame.SearchPanel.filteredIDs) then
-			filterTable(LFGListFrame.SearchPanel.results, LFGListFrame.SearchPanel.filteredIDs)
-			LFGListFrame.SearchPanel.filteredIDs = nil
-			
-			LFGListSearchPanel_UpdateResults(LFGListFrame.SearchPanel)
-			--print('Removed: '..countRemoved..', leaving '..countRemaining)
-		end
-	end
+	RF.togRemove = not RF.togRemove
+	LFGListSearchPanel_UpdateResultList (LFGListFrame.SearchPanel)
+	LFGListSearchPanel_UpdateResults 	(LFGListFrame.SearchPanel)
 end
-
-function RF.Dialog_UseRF_OnClick(self, button, down)
-    local checked = self:GetChecked()
-    RF.togRemove = checked
-    LFGListSearchPanel_DoSearch(LFGListFrame.SearchPanel)
-end
-
-function RF.Dialog_SetUpRFCheckbox()
-    local button = CreateFrame("CheckButton", "UseRFButton", LFGListFrame.SearchPanel, "UICheckButtonTemplate")
-    button:SetSize(26, 26)
-    button:SetHitRectInsets(-2, -30, -2, -2)
-    button.text:SetText("RF")
-    button.text:SetFontObject("GameFontHighlight")
-    button.text:SetWidth(30)
-    button:SetPoint("LEFT", LFGListFrame.SearchPanel.RefreshButton, "LEFT", -62, 0)
-    button:SetPoint("TOP", LFGListFrame.SearchPanel.RefreshButton, "TOP", 0, -3)
-    button:SetScript("OnClick", RF.Dialog_UseRF_OnClick)
-    button:SetScript("OnEnter", function (self)
-        GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
-        GameTooltip:SetText("Enable or disable the local region filter")
-    end)
-    button:SetScript("OnLeave", function () GameTooltip:Hide() end)
-    RF.UseRFButton = button
-end
-
-
--- SLASH_RFILTER1 = "/rfilter"
--- SlashCmdList["RFILTER"] = function(msg)
--- 	if RF.togRemove then
--- 		print('|cff00ffff[Region Filter]: |cffFF6EB4 Not filtering outside regions')
--- 	else
--- 		print('|cff00ffff[Region Filter]: |cffFF6EB4 Filtering outside regions')
--- 	end
--- 	RF.togRemove = not RF.togRemove
--- 	LFGListSearchPanel_UpdateResultList (LFGListFrame.SearchPanel)
--- 	LFGListSearchPanel_UpdateResults 	(LFGListFrame.SearchPanel)
--- end
 
 ---- Print When Loaded ----
 local welcomePrompt = CreateFrame("Frame")
@@ -205,12 +128,9 @@ welcomePrompt:RegisterEvent("PLAYER_LOGIN")
 welcomePrompt:SetScript("OnEvent", function(_, event)
 	if event == "PLAYER_LOGIN" then
 		print("|cff00ffff[Region Filter]|r |cffffcc00Version "..RF.version.."|r. If there any bugs please report them at https://github.com/jamesb93/RegionFilter")
-		print("|cff00ffff[Region Filter]|r If possible, stop using CurseForge (soon/now to be Overwolf) and try CurseBreaker https://www.github.com/AcidWeb/CurseBreaker.")
 		print(RF.postType)
 	end
 end)
 
-RF.Dialog_SetUpRFCheckbox()
--- hooksecurefunc("LFGListUtil_SortSearchResults", RF.sortEntries)
-hooksecurefunc("LFGListSearchEntry_Update", 	RF.updateEntries)
-hooksecurefunc("LFGListUtil_SortSearchResults", RF.sortSearchResults);
+hooksecurefunc("LFGListUtil_SortSearchResults", RF.removeEntries)
+hooksecurefunc("LFGListSearchEntry_Update", RF.updateEntries)
